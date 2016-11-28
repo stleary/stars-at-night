aa10f3783461e10d9f22973cfd5f22d5c2abd6ec
stleary <stleary@gmail.com>
2016-11-27 19:14:32 -0600

    set table style and output. Add astronomical twilight values

--- src/NightSkyManager.php
+++ src/NightSkyManager.php
@@ -1,4 +1,4 @@
-<?php 
+<?php  
 
 /*
     This program is free software; you can redistribute it and/or modify
@@ -51,7 +51,27 @@ class NightSkyManager {
     }
     
     public function enqueuestyles() {
-        // wp_enqueue_style( 'sunrisesunsetcss', plugins_url('../css/sunrisesunset.css',__FILE__), array(), $this->version  );
+        wp_enqueue_style( 'nightskycss', plugins_url('../css/NightSky.css',__FILE__), array(), $this->version  );
+    }
+
+    /**
+     * This method can be used to calculate early morning or late evening
+     * astronomical twilight. By definition this is 90 minutes before sunrise
+     * or 90 minutes after sunset.
+     * $today - day for which the calculation is being made
+     * $tzOffset - timezone offset in seconds
+     * $sunTime - either sunrise or sunset, string hh:mm 24 hr format
+     * $delta - use -90 for morning, +90 for evening
+     * return: twilight string in hh:mm format
+     */
+    public function calculateTwilight($today, $tzOffset, $sunTime, $delta) {
+        $todayStr = $today . " " . $sunTime;
+        $todayTimestamp = strtotime($todayStr);
+        $twilight = $todayTimestamp + $delta + tzOffset;
+        $dateTime = new DateTime();
+        $dateTime = $dateTime->setTimestamp($twilight);
+        $twilightStr = $dateTime->format('H:i');
+        return $twilightStr;
     }
     
     public function runNightSky($atts) {
@@ -76,13 +96,16 @@ class NightSkyManager {
         $tzOffset = $remote_dtz->getOffset($remote_dt) / 3600;
                 
         // returns a string like this: 07:10
-        $sunrisedata = date_sunrise(strtotime('now'), SUNFUNCS_RET_STRING, $lat, $long, $zenith, $tzOffset);
-        $sunsetdata = date_sunset(strtotime('now'), SUNFUNCS_RET_STRING, $lat, $long, $zenith, $tzOffset);
+        $sunRise = date_sunrise(strtotime('now'), SUNFUNCS_RET_STRING, $lat, $long, $zenith, $tzOffset);
+        $sunSet = date_sunset(strtotime('now'), SUNFUNCS_RET_STRING, $lat, $long, $zenith, $tzOffset);
 
         $year = $remote_dt->format('Y');
         $month = $remote_dt->format('m');
         $day = $remote_dt->format('d');
         $today = $remote_dt->format('D m/d/Y');
+
+        $morningTwilight = $this->calculateTwilight($today, $tzOffset, $sunRise, (-90 * 60));
+        $eveningTwilight = $this->calculateTwilight($today, $tzOffset, $sunSet, (90 * 60));
 
         include('moon.php');
         $moonData = Moon::calculateMoonTimes($month, $day, $year, $lat, $long);
@@ -101,32 +124,55 @@ class NightSkyManager {
             $moonSet = "None";
         }
 
-        // $this->display_sunrisesunset( $sunrisedata, $sunsetdata, $moonRise, $moonSet, $graphical == 'true' );
-        print "COE (30.8910, -98.4265) astronomical times for today ($today)<br>";
-        print "Sunrise: $sunrisedata<br>";    
-        print "Sunset: $sunsetdata<br>";    
-        print "Moonrise: $moonRise<br>";
-        print "Moonset: $moonSet<br><p>";
+        $this->display( $lat, $long, $today, $sunRise, $sunSet, $moonRise, $moonSet, $morningTwilight, $eveningTwilight );
         return;
     }
 
     /*
-        Displays sunrise/sunset data:
-        $sunrise - time value for sunrise (eg. 6:00)
-        $sunset - time value for sunset (eg. 15:00)
-        $graphical - bool true to display sun/moon with output.
+        Displays night sky data:
+        $lat, $long - location
+        $today - time
+        $sunRise - time value for sunrise (eg. 6:00)
+        $sunSet - time value for sunset (eg. 15:00)
+        $moonRise - time value for Moonrise (eg. 6:00)
+        $moonSet - time value for Moonset (eg. 15:00)
+        $morningTwilight - morning astronomical twilight
+        $eveningTwilight - evening astronomical twilight
     */    
-    public function display_sunrisesunset($sunrise, $sunset, $moonRise, $moonSet, $graphical = true) {
+    public function display($lat, $long, $today, $sunRise, $sunSet, $moonRise, $moonSet, $morningTwilight, $eveningTwilight) {
     ?>
-        <div class="sunrise-sunset<?php echo ( $graphical == true? ' sun-graphical':'');?>">
-            <span class="time-sunrise"><strong>Sunrise</strong><?php echo date('g:i a', strtotime( date('n/j/Y', strtotime('now')) . $sunrise )); ?></span>
-            <span class="time-sunset"><strong>Sunset</strong><?php echo date('g:i a', strtotime( date('n/j/Y', strtotime('now')) . $sunset )); ?></span>
-            <span class="time-moonrise"><strong>Moonrise</strong><?php echo date('g:i a', strtotime( date('n/j/Y', strtotime('now')) . $moonRise )); ?></span>
-            <span class="time-moonset"><strong>Moonset</strong><?php echo date('g:i a', strtotime( date('n/j/Y', strtotime('now')) . $moonSet )); ?></span>
+        <div class="nightsky">
+           <bold>COE (<?php echo $lat ?>, <?php echo $long ?>) astronomical times for today (<?php echo $today ?>)</bold>
+           <table>
+                <tr>
+                <td>Astronomical twilight</td>
+                <td><?php echo $morningTwilight ?></td>
+                </tr>
+                <tr>
+                <td>Sunrise</td>
+                <td><?php echo $sunRise ?></td>
+                </tr>
+                <tr>
+                <td>Sunset</td>
+                <td><?php echo $sunSet ?></td>
+                </tr>
+                <tr>
+                <td>Astronomical twilight</td>
+                <td><?php echo $eveningTwilight ?></td>
+                </tr>
+                <tr>
+                <td>Moonrise</td>
+                <td><?php echo $moonRise ?></td>
+                </tr>
+                <tr>
+                <td>Moonset</td>
+                <td><?php echo $moonSet ?></td>
+                </tr>
+            </table>    
         </div>
         <?php
             
         return;
 
     }
-}
+} 
